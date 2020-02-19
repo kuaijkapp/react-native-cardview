de924a53e799e31c2fb075a725dc3204045c8cbc
xiangzhichao <451626253@qq.com>
2020-02-19 13:02:44 +0800

    适配androidx

--- android/src/main/java/com/kishanjvaghela/cardview/RNCardView.java
+++ android/src/main/java/com/kishanjvaghela/cardview/RNCardView.java
@@ -1,7 +1,7 @@
 package com.kishanjvaghela.cardview;
 
 import android.content.Context;
-import androidx.cardview.widget.CardView;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 
 import com.facebook.react.uimanager.PixelUtil;
